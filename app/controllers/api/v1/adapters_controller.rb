c60cc876b01284486f3800de9e0fcc43f2d374e5
tsgreenberg1217 <tsgreenberg1217@gmail.com>
2017-12-05 15:47:25 -0500

    errors

--- app/controllers/api/v1/adapters_controller.rb
+++ app/controllers/api/v1/adapters_controller.rb
@@ -6,6 +6,7 @@ class Api::V1::AdaptersController <  ActionController::API
   end
 
   def create
+    byebug
     search = Search.new(params[:lat], params[:lng])
     render json: search.results
   end
