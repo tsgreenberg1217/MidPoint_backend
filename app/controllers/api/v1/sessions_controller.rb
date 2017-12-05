3f4cd5154ae17e628a4850e6a93e3a499441fc33
tsgreenberg1217 <tsgreenberg1217@gmail.com>
2017-12-05 15:26:36 -0500

    pulling

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -9,7 +9,6 @@ class Api::V1::SessionsController < ApplicationController
   end
 
   def create
-
     user = User.find_by(username: params[:username])
     if user && user.authenticate(params[:password])
       payload = {user_id: user.id}
