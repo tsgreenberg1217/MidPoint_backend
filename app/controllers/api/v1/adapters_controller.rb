bf03a14f29d54bb98f076b94f407bcb61010c53e
tsgreenberg1217 <tsgreenberg1217@gmail.com>
2017-12-06 16:52:56 -0500

    ready to merge to master

--- app/controllers/api/v1/adapters_controller.rb
+++ app/controllers/api/v1/adapters_controller.rb
@@ -7,8 +7,6 @@ class Api::V1::AdaptersController <  ActionController::API
   end
 
   def create
-    # byebug  
-    # search = Search.new(params[:lat], params[:lng])
     location = "#{params[:lat]}, #{params[:lng]}"
     term = "#{params[:term]}"
     results = YelpAPI.test(location, term)
